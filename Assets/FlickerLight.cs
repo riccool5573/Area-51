fc29d3a4769b4596397b5159c7f5d67560c25206
BOOSTEDmati <25820@ma-web.nl>
2020-05-15 11:52:18 +0200

    flickering light

---
+++ Assets/FlickerLight.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickerLight : MonoBehaviour
+{
+    public bool isflickering = false;
+    public float timeDelay;
+
+
+    void Update()
+    {
+        if (!isflickering)
+        {
+            StartCoroutine(FlickeringLight());
+        }
+    }
+
+    IEnumerator FlickeringLight()
+    {
+        isflickering = true;
+        this.gameObject.GetComponent<Light>().intensity = 2;
+        timeDelay = Random.Range(0.07f, 0.25f);
+        yield return new WaitForSeconds(timeDelay);
+        this.gameObject.GetComponent<Light>().intensity = 1;
+        timeDelay = Random.Range(0.07f, 0.25f);
+        yield return new WaitForSeconds(timeDelay);
+        this.gameObject.GetComponent<Light>().intensity = 2;
+        isflickering = false;
+    }
+}
