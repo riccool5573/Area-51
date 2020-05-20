2fcb2ef1e99d1fce3042a198cd4ce35ba90be7cc
BOOSTEDmati <25820@ma-web.nl>
2020-05-20 09:23:11 +0200

    playerdeath

---
+++ Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.CompareTag("Enemy"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    
+    
+
+
+
+
+
+
+}
